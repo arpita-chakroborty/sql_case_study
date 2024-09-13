cf5bd4a12dd70e5c53f9fc4d49dbc1fe564f9ae5
Arpita Chakroborty <arpitachakroborty.official@gmail.com>
2024-09-13 12:25:29 +0530

    Update BIKES SALES DATA QUESTION.sql

--- SAP_BIKES_SALES /BIKES SALES DATA QUESTION.sql
+++ SAP_BIKES_SALES /BIKES SALES DATA QUESTION.sql
@@ -1,1 +1,47 @@
 
+- Products Analysis
+-- Q1: Find the total number of products for each product category
+-- Q2: List the top 5 most expensive products
+-- Q3: Find all products that belong to the 'Mountain Bike' category
+-- Q4: List the total sales amount (gross) for each product category
+-- Q7: List the top 5 suppliers by total product sales.
+-- Q6: Find the total number of products created by each employee
+-- Q7: List the employees who have changed product details the most
+
+
+-- Sales Orders Items Analysis
+-- Q8: Calculate the total gross amount for each sales order.
+-- Q9: Trend in sales over different fiscal year periods
+-- Q10: Which products contribute the most to revenue when the billing status is 'Complete'
+-- Q11: Find the sales order items for a specific product ID.
+
+
+-- Sales Orders Analysis
+-- Q12: How does revenue vary over different fiscal year periods for orders with a billing status of 'Complete'
+-- Q13: Find the top-selling product within each category along with its total sales amount.
+-- Q14: Calculate the total gross amount for each sales organization.
+-- Q15: Find the top 5 sales orders by net amount.
+-- Q16: How many sales orders were created in the year 2018?
+
+-- Business Partners Analysis
+-- Q17: How many business partners are there for each partner role?
+-- Q18: List the top 5 companies with the most recent creation dates.
+
+-- Employees Analysis
+-- Q19: Find the number of employees for each sex.
+-- Q20: List the employees who have 'W' in their first name 
+
+-- Product Categories Analysis
+-- Q21: List all product categories along with their descriptions.
+-- Q22: Find all products that belong to the 'Mountain Bike' category.
+
+-- Addresses Analysis
+-- Q23: Count the number of addresses in each country.
+-- Q24: List all addresses in a specific city, e.g., 'New York'.
+-- Q25: Find the number of business partners in each region.
+-- Q26: List all addresses associated with a specific business partner.
+
+
+-- Combining Data from Multiple Tables
+-- Q27: List the top 5 employees who have created the most sales orders.
+-- Q28: Find the total sales amount (gross) for each product category.
